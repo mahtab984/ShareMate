a354a73e1002ff400962e222917875a3de1c3555
Mahtab Pouradam <C0546948@camosun.ca>
2025-06-19 16:10:12 -0700

    added variables and methods to class GroupAdmin

--- ShareMate/src/model/GroupAdmin.java
+++ ShareMate/src/model/GroupAdmin.java
@@ -4,10 +4,36 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author c0546948
  */
 public class GroupAdmin extends User {
     
+
+    
+    
+ public GroupAdmin(int userID, String name, String email, String passwordHash, Date joinDate){
+ super(userID,name,email,passwordHash,joinDate);
+ 
+ }
+ 
+ public void createRecurringExpense(Expense template){
+ System.out.println("recurring expense created"+ template);
+ }
+ 
+ public void lockExpense( int expenseID){
+ System.out.println("Expense "+ expenseID+ "locked");
+ } 
+ 
+ public void approveExpense( int expenseID){
+ System.out.println("Expense "+ expenseID+ "approved");
+ 
+ 
+ } 
+ 
+ //add manageGroupMembers method
+ 
 }
