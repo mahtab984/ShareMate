0662caea0b69fb1c08ad01ce613ee5a8102265a8
Mahtab Pouradam <C0546948@camosun.ca>
2025-06-19 15:32:08 -0700

    added variables and methods to class User and class Expense

--- ShareMate/src/model/Expense.java
+++ ShareMate/src/model/Expense.java
@@ -3,11 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+  import java.util.*;
 
 /**
  *
  * @author c0546948
  */
 public class Expense {
+  
+
+    private int expenseID;
+    private float amount;
+    private String description;
+    private Date date;
+    private boolean isLocked;
+    private boolean isApproved;
+    private User createdBy;
+    private String filePath;
+
+    public Expense(int expenseID, float amount, String description, Date date, boolean isLocked, boolean isApproved, User createdBy) {
+        this.expenseID = expenseID;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+        this.isLocked = isLocked;
+        this.isApproved = isApproved;
+        this.createdBy = createdBy;
+    }
+
+     public void attachReceipt(String filepath) {
+        // Simulate attaching a receipt
+        System.out.println("Receipt attached from: " + filePath);
+     }
+
+    public void markAsApproved() {
+        this.isApproved = true;
+        System.out.println("Expense marked as approved.");
+    }
+
+    public void markAsLocked() {
+        this.isLocked = true;
+        System.out.println("Expense marked as locked.");
+    }
+
     
 }
--- ShareMate/src/model/User.java
+++ ShareMate/src/model/User.java
@@ -4,10 +4,43 @@
  */
 package model;
 
+import java.util.*;
+
 /**
  *
  * @author c0546948
  */
 public class User {
+
+    private int userID;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private Date joinDate;
+
+    public User(int userID, String name, String email, String passwordHash, Date joinDate) {
+        this.userID = userID;
+        this.name = name;
+        this.passwordHash = passwordHash;
+        this.joinDate = joinDate;
+    }
+
+    public void addExpence(Expense expense) {
+        System.out.println("Expense added: " + expense);
+    }
+
+    //add viewExpenseHistory method
+    
+    
+    public void updateProfile(String name, String email) {
+        this.name = name;
+        this.email = email;
+        System.out.println("profile updated");
+    }
+
+    public void receiveNotification (Notification notification){
+    System.out.println("Notification: "+ notification );
+    }
+    
     
 }
