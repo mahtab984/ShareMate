b020c8ea88e7003d08718ca51c37b7f95f5553bc
Oscartofan <ofvarelarevelo@gmail.com>
2025-06-26 15:26:18 -0700

    Update DBConnection.java

--- ShareMate/src/utility/DBConnection.java
+++ ShareMate/src/utility/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/sharemate";
     private static final String USER = "root";
-    private static final String PASSWORD = "ShareMate_DB**";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
