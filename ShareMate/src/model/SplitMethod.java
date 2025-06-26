89972bc2e9ad4b0a1e1efa8f21bc39827c03283c
Oscartofan <ofvarelarevelo@gmail.com>
2025-06-26 01:20:20 -0700

    Update SplitMethod.java

--- ShareMate/src/model/SplitMethod.java
+++ ShareMate/src/model/SplitMethod.java
@@ -3,16 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-  import java.util.*;
 
-/**
- * Represents a split method for dividing expenses among users.
- * 
- * @author c0546948
- */
+
+
+// SplitMethod.java
 public class SplitMethod {
-
-    private int methodID;
+    private int methodId;
     private String type;
-   
+    private String parameters;
+
+    public SplitMethod() { }
+
+    public SplitMethod(int methodId, String type, String parameters) {
+        this.methodId   = methodId;
+        this.type       = type;
+        this.parameters = parameters;
+    }
+
+    // Getters & setters
+    public int getMethodId() { return methodId; }
+    public void setMethodId(int methodId) { this.methodId = methodId; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getParameters() { return parameters; }
+    public void setParameters(String parameters) { this.parameters = parameters; }
+
+    @Override
+    public String toString() {
+        return "SplitMethod{" +
+               "methodId=" + methodId +
+               ", type='" + type + '\'' +
+               ", parameters='" + parameters + '\'' +
+               '}';
+    }
 }
